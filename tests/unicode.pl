77dd44417f79c9f5658ea6be896290ea67d057bc
Alhadis <gardnerjohng@gmail.com>
2017-10-22 16:41:30 +1100

    Add pattern-matching for @arrays and %hashes

--- tests/unicode.pl
+++ tests/unicode.pl
@@ -2,6 +2,8 @@ my $scalar   = $days;
 my $arrayRef = $days[28];
 my $hashRef  = $days{"Feb"};
 
+
+# Scalar terms
 ${days}             ; # Same as $days but unambiguous before alphanumerics
 my ${               ; # This may span multiple lines:
 	otherValue
@@ -16,6 +18,17 @@ $days->[28]         ; # 29th element of array pointed to by reference $days
 $days[0][2]         ; # Multidimensional array
 $days{2000}{"Feb"}  ; # Multidimensional hash
 $days{2000,"Feb"}   ; # Multidimensional hash emulation
+
+
+# List terms
+@days               ; # Array containing ($days[0], $days[1], ... $days[N])
+@days[3, 4, 5]      ; # Array slice containing ($days[3], $days[4], $days[5])
+@days[3..5]         ; # Array slice containing ($days[3], $days[4], $days[5])
+@days{"Jan","Feb"}  ; # Hash slice containing ($days{"Jan"},$days{"Feb"})
+
+
+# Hash terms
+%days = (Jan => 31, Feb => $leap ? 29 : 28);
 
 
 # Unicode tests
